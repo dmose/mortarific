c8cf397bdb3571d8e6453452a14fad8d7cb0ccf5
Dan Mosedale <dmose@meer.net>
2012-07-11 13:18:53 -0700

    Initial commit of WebGameStub generated template

---
+++ templates/WebGameStub/blocks.js
@@ -0,0 +1,17 @@
+
+var fs = require('fs');
+
+module.exports = {
+    head: function() {
+        return fs.readFileSync(__dirname + '/head.html');
+    },
+    js_init: function() {
+        return fs.readFileSync(__dirname + '/js_init.html');
+    },
+    js_global: function() {
+        return fs.readFileSync(__dirname + '/js_global.html');
+    },
+    js_requires: function() {
+        return fs.readFileSync(__dirname + '/js_requires.html');
+    }
+};
